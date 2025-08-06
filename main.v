0fe8f6455938c022a73b55872165152a8f9e9457
Nirei <nireiorange@gmx.com>
2025-08-06 02:34:54 +0200

    feat: implement image rendering with stbi

--- main.v
+++ main.v
@@ -1,6 +1,81 @@
 module main
 
-const dots_to_braille = [0, 3, 1, 4, 2, 5, 6, 7]
+import stbi
+
+pub fn Canvas.new(width int, height int) &Canvas {
+	return &Canvas { layers: create_buffer(width, height) }
+}
+
+pub fn (canvas Canvas) size() (int, int) {
+	return canvas.layers[0].len, canvas.layers.len
+}
+
+pub fn (canvas Canvas) output() [][]rune {
+	return buffer_to_braille(canvas.layers, dots_to_braille_rune_map)
+}
+
+pub fn (mut canvas Canvas) set(x int , y int) {
+	canvas.layers[y][x] = true
+}
+
+pub fn (mut canvas Canvas) unset(x int, y int) {
+	canvas.layers[y][x] = false
+}
+
+pub fn (mut canvas Canvas) clear() {
+	width, height := canvas.size()
+	canvas.layers = create_buffer(width, height)
+}
+
+pub fn (mut canvas Canvas) image(image stbi.Image) ! {
+	width, height := canvas.size()
+	length := width * height
+	resized := stbi.resize_uint8(image, width, height)!
+	mut floating := []f64{len: length}
+	unsafe {
+		for index in 0..length { floating[index] = f64(resized.data[index]) / 255.0  }
+	}
+
+	for y in 0..height {
+		for x in 0..width {
+			index := y * width + x
+			if floating[index] > 0.5 { canvas.set(x, y) }
+
+			if y == height - 1 || x == 0 || x == 1 || x == width - 1 || x == width - 2 { continue }
+
+			// Burkes dithering
+			new_pixel := if floating[index] > 0.5 { 1.0 } else { 0.0 } 
+			difference := floating[index] - new_pixel
+			floating[index+1] 			+= 8.0/32.0 * difference
+			floating[index+2] 			+= 4.0/32.0 * difference
+			floating[index+width-2] += 2.0/32.0 * difference
+			floating[index+width-1] += 4.0/32.0 * difference
+			floating[index+width] 	+= 8.0/32.0 * difference
+			floating[index+width+1] += 4.0/32.0 * difference
+			floating[index+width+1] += 2.0/32.0 * difference
+		}
+	}
+}
+
+/* private code */
+
+struct Canvas {
+	mut:
+		layers [][]bool
+}
+
+const grayscale_ratio_red = 0.2126
+const grayscale_ratio_green = 0.7152
+const grayscale_ratio_blue = 0.0722
+/**
+ * Maps each bit number in a dots byte (western reading order) to it's
+ * corresponding bit number in the unicode Braille mapping.
+ */
+const dots_to_braille = [u8(0), 3, 1, 4, 2, 5, 6, 7]
+/**
+ * Maps each possible dots byte to an unicode rune.
+ */
+const dots_to_braille_rune_map = memoize_dots_to_braille_rune()
 
 /**
  * dots_to_braille_rune returns the Unicode Braille character for a given 8-dot
@@ -82,30 +157,52 @@ fn buffer_to_braille(buffer [][]bool, braille_mapping [256]rune) [][]rune {
   return output
 }
 
-fn main() {
-  braille_mapping := memoize_dots_to_braille_rune()
-	mut buffer := [][]bool{len: 48, init: []bool{len: 40}}
+fn create_buffer(width int, height int) [][]bool {
+ return [][]bool{len: height, init: []bool{len: width}}
+}
+
+fn draw_crescent() &Canvas {
+  width, height := 48, 48
+  mut canvas := Canvas.new(width, height)
 
   minr := 16*16
   maxr := 20*20
-	for y:=0; y<48; y+=1 {
-		for x:=0; x<40; x+=1 {
+	for y:=0; y<height; y+=1 {
+		for x:=0; x<width; x+=1 {
       cy := y - 24
       big_cx := x - 20
       small_cx := x - 16
       big := (big_cx*big_cx) + (cy*cy)
       small := (small_cx*small_cx) + (cy*cy)
       if small > minr && big < maxr {
-        buffer[y][x] = true
+        canvas.set(x, y)
       }
 		}
 	}
 
-  output := buffer_to_braille(buffer, braille_mapping)
-  for row in output {
-    for character in row {
-      print(character)
-    }
-    print('\n')
-  }
+	return canvas
+}
+
+fn draw_wolf() &Canvas {
+	width, height := 320, 180
+  mut canvas := Canvas.new(width, height)
+
+	image := stbi.load("./spice-and-wolf.jpg", stbi.LoadParams{desired_channels: 1}) or { exit(1) }
+
+	canvas.image(image) or { exit(1) }
+	image.free()
+
+	return canvas
+}
+
+fn main() {
+  canvas := draw_wolf()
+
+  output := canvas.output()
+	for row in output {
+		for character in row {
+			print(character)
+		}
+		print('\n')
+	}
 }
