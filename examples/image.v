43a70b78e38923dbeab9334534a72ab77b8a6b0c
Nirei <nireiorange@gmx.com>
2025-09-27 14:27:57 +0200

    fix: image example

--- examples/image.v
+++ examples/image.v
@@ -5,7 +5,7 @@ fn draw_wolf() &vrawille.Canvas {
 	width, height := 320, 180
   mut canvas := vrawille.Canvas.new(width, height)
 
-	image := stbi.load("./spice-and-wolf.jpg", stbi.LoadParams{desired_channels: 1}) or { exit(1) }
+	image := stbi.load("examples/spice-and-wolf.jpg", stbi.LoadParams{desired_channels: 1}) or { exit(1) }
 
 	canvas.image(image) or { exit(1) }
 	image.free()
