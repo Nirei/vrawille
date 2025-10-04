492d5498d513f06e27099f7d1d05b5811132ac4b
Nirei <nireiorange@gmx.com>
2025-10-04 03:57:09 +0200

    fix: correct example to use degrees not radians

--- examples/polygon.v
+++ examples/polygon.v
@@ -1,13 +1,13 @@
 import nirei.vrawille
 
 /**
- * Draws an heptagon X through `canvas.polygon()`
+ * Draws an heptagon through `canvas.polygon()`
  */
 fn main() {
   width, height := 80, 60
   mut canvas := vrawille.Canvas.new(width, height)
 
-	canvas.polygon(30,30,7,50,3.1416/2)
+	canvas.polygon(30,30,7,50,90)
 	
 	println(*canvas)
 }
